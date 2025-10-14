4df5121a76c54b2cdefaeef4e03cb23bc9e9bbf4
hanhly31231021229 <hanhly.31231021229@st.ueh.edu.vn>
2025-10-14 18:41:32 +0700

    Update Form1.Designer.cs

--- BT4/Form1.Designer.cs
+++ BT4/Form1.Designer.cs
@@ -36,104 +36,68 @@
             btnChia = new Button();
             btnDel = new Button();
             SuspendLayout();
-            // 
-            // lblSo1
-            // 
             lblSo1.AutoSize = true;
             lblSo1.Location = new Point(30, 30);
             lblSo1.Name = "lblSo1";
             lblSo1.Size = new Size(85, 20);
             lblSo1.TabIndex = 0;
             lblSo1.Text = "Số thứ nhất";
-            // 
-            // lblSo2
-            // 
             lblSo2.AutoSize = true;
             lblSo2.Location = new Point(30, 70);
             lblSo2.Name = "lblSo2";
             lblSo2.Size = new Size(76, 20);
             lblSo2.TabIndex = 1;
             lblSo2.Text = "Số thứ hai";
-            // 
-            // lblKetQua
-            // 
             lblKetQua.AutoSize = true;
             lblKetQua.Location = new Point(30, 160);
             lblKetQua.Name = "lblKetQua";
             lblKetQua.Size = new Size(60, 20);
             lblKetQua.TabIndex = 2;
             lblKetQua.Text = "Kết quả";
-            // 
-            // txtSo1
-            // 
             txtSo1.Location = new Point(120, 27);
             txtSo1.Name = "txtSo1";
             txtSo1.Size = new Size(180, 27);
             txtSo1.TabIndex = 3;
             txtSo1.TextChanged += txtSo1_TextChanged;
-            // 
-            // txtSo2
-            // 
             txtSo2.Location = new Point(120, 67);
             txtSo2.Name = "txtSo2";
             txtSo2.Size = new Size(180, 27);
             txtSo2.TabIndex = 4;
-            // 
-            // txtKetQua
-            // 
             txtKetQua.Location = new Point(120, 157);
             txtKetQua.Name = "txtKetQua";
             txtKetQua.ReadOnly = true;
             txtKetQua.Size = new Size(180, 27);
             txtKetQua.TabIndex = 5;
-            // 
-            // btnCong
-            // 
             btnCong.Location = new Point(30, 110);
             btnCong.Name = "btnCong";
             btnCong.Size = new Size(40, 30);
             btnCong.TabIndex = 6;
             btnCong.Text = "+";
             btnCong.UseVisualStyleBackColor = true;
-            // 
-            // btnTru
-            // 
             btnTru.Location = new Point(80, 110);
             btnTru.Name = "btnTru";
             btnTru.Size = new Size(40, 30);
             btnTru.TabIndex = 7;
             btnTru.Text = "-";
             btnTru.UseVisualStyleBackColor = true;
-            // 
-            // btnNhan
-            // 
             btnNhan.Location = new Point(130, 110);
             btnNhan.Name = "btnNhan";
             btnNhan.Size = new Size(40, 30);
             btnNhan.TabIndex = 8;
             btnNhan.Text = "*";
             btnNhan.UseVisualStyleBackColor = true;
-            // 
-            // btnChia
-            // 
             btnChia.Location = new Point(180, 110);
             btnChia.Name = "btnChia";
             btnChia.Size = new Size(40, 30);
             btnChia.TabIndex = 9;
             btnChia.Text = "/";
             btnChia.UseVisualStyleBackColor = true;
-            // 
-            // btnDel
-            // 
             btnDel.Location = new Point(240, 110);
             btnDel.Name = "btnDel";
             btnDel.Size = new Size(60, 30);
             btnDel.TabIndex = 10;
             btnDel.Text = "Del";
             btnDel.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
             ClientSize = new Size(340, 210);
             Controls.Add(lblSo1);
             Controls.Add(lblSo2);
@@ -153,3 +117,4 @@
         }
     }
 }
+
