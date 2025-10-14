d158be1559474ed8381badbee88d31219a36814c
hanhly31231021229 <hanhly.31231021229@st.ueh.edu.vn>
2025-10-14 18:36:41 +0700

    Add files via upload

---
+++ BT3/Form1.cs
@@ -0,0 +1,73 @@
+﻿namespace BT3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnHienThi.Click += BtnHienThi_Click;
+            btnThoat.Click += BtnThoat_Click;
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+            this.MouseClick += Form1_MouseClick;
+            this.Load += Form1_Load;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có muốn mở ứng dụng không?",
+                "Xác nhận",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.No)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void BtnHienThi_Click(object sender, EventArgs e)
+        {
+            string noiDung = txtNoiDung.Text;
+            MessageBox.Show(noiDung, "Nội dung TextBox");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.KeyCode == Keys.H)
+            {
+                txtNoiDung.Text = "Xin chào Khoa CNTTKD";
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+                MessageBox.Show("Bạn vừa nhấp chuột phải!");
+            else if (e.Button == MouseButtons.Left)
+                MessageBox.Show("Bạn vừa nhấp chuột trái!");
+            else if (e.Button == MouseButtons.Middle)
+                MessageBox.Show("Bạn vừa nhấp chuột giữa!");
+        }
+
+        private void BtnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Bạn có muốn thoát ứng dụng không?",
+                "Xác nhận thoát",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        private void txtNoiDung_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BT4/Form1.cs
@@ -0,0 +1,79 @@
+﻿namespace BT4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSo1_TextChanged(object sender, EventArgs e)
+        {
+            // Gán sự kiện cho các nút
+            btnCong.Click += BtnCong_Click;
+            btnTru.Click += BtnTru_Click;
+            btnNhan.Click += BtnNhan_Click;
+            btnChia.Click += BtnChia_Click;
+            btnDel.Click += BtnDel_Click;
+
+            // Gán sự kiện kiểm tra nhập liệu
+            txtSo1.KeyPress += TxtSo_KeyPress;
+            txtSo2.KeyPress += TxtSo_KeyPress;
+        }
+        private void TxtSo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; // chặn ký tự không hợp lệ
+            }
+        }
+
+        private void BtnCong_Click(object sender, EventArgs e)
+        {
+            if (TryGetValues(out double a, out double b))
+                txtKetQua.Text = (a + b).ToString();
+        }
+
+        private void BtnTru_Click(object sender, EventArgs e)
+        {
+            if (TryGetValues(out double a, out double b))
+                txtKetQua.Text = (a - b).ToString();
+        }
+
+        private void BtnNhan_Click(object sender, EventArgs e)
+        {
+            if (TryGetValues(out double a, out double b))
+                txtKetQua.Text = (a * b).ToString();
+        }
+
+        private void BtnChia_Click(object sender, EventArgs e)
+        {
+            if (TryGetValues(out double a, out double b))
+            {
+                if (b == 0)
+                    MessageBox.Show("Không thể chia cho 0!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                    txtKetQua.Text = (a / b).ToString();
+            }
+        }
+
+        private void BtnDel_Click(object sender, EventArgs e)
+        {
+            txtSo1.Clear();
+            txtSo2.Clear();
+            txtKetQua.Clear();
+        }
+
+        private bool TryGetValues(out double a, out double b)
+        {
+            bool ok1 = double.TryParse(txtSo1.Text, out a);
+            bool ok2 = double.TryParse(txtSo2.Text, out b);
+            if (!ok1 || !ok2)
+            {
+                MessageBox.Show("Vui lòng nhập hai số hợp lệ!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+    }
+}
