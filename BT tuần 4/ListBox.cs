bc31958b1d9f6bf2477b46db7e615e8cd3fb3544
hanhly31231021229 <hanhly.31231021229@st.ueh.edu.vn>
2025-11-04 20:05:20 +0700

    Update ListBox.cs

--- BT tuần 4/ListBox.cs
+++ BT tuần 4/ListBox.cs
@@ -11,7 +11,7 @@ namespace BT_tuần_4
     public partial class ListBox : Form
     {
         // Chuỗi kết nối
-        string strConnectionString = "Data Source = LAPTOP-Q7DB2USJ\\SQLEXPRESS; Initial Catalog = QLBH; Integrated Security=True; User Id =sa; Password = 123456";
+        string strConnectionString = "Data Source = LAPTOP-Q7DB2USJ\\SQLEXPRESS; Initial Catalog = QLBH; Integrated Security=True;TrustServerCertificate=True; User Id =sa; Password = 123456";
         // Đối tượng kết nối dữ liệu
         SqlConnection conn = null;
         // Đối tượng thực hiện vận chuyển dữ liệu  
@@ -76,6 +76,7 @@ namespace BT_tuần_4
         }
     }
 }
+
 
 
 
