b04ecf93c8698ffc7719016541ee0edd92a3c333
hanhly31231021229 <hanhly.31231021229@st.ueh.edu.vn>
2025-11-04 20:35:58 +0700

    Add files via upload

---
+++ BT tuần 4/AboutForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BT_tuần_4
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblInfo;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblInfo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblInfo.Text = "Tên: Lý Hồ Hiếu Hạnh\nMSSV: 31231021229";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblInfo.AutoSize = false;
+
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "About";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ BT tuần 4/AboutForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT_tuần_4
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+            this.Text = "About";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.ClientSize = new Size(400, 200);
+            this.BackColor = Color.White;
+
+            Label lblInfo = new Label();
+            lblInfo.Text = "Tên: Lý Hồ Hiếu Hạnh\nMSSV: 31231021229";
+            lblInfo.Font = new Font("Segoe UI", 12F, FontStyle.Regular);
+            lblInfo.TextAlign = ContentAlignment.MiddleCenter;
+            lblInfo.Dock = DockStyle.Fill;
+
+            this.Controls.Add(lblInfo);
+        }
+
+    }
+     
+}
